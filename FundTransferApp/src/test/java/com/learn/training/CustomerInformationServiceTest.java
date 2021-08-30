cbddc3f7dfc5d8f367770dcd1f28a1f43e7a7cc2
Vineethrao-77 <88935971+Vineethrao-77@users.noreply.github.com>
2021-08-30 15:49:32 +0530

    Update CustomerInformationServiceTest.java

--- FundTransferApp/src/test/java/com/learn/training/CustomerInformationServiceTest.java
+++ FundTransferApp/src/test/java/com/learn/training/CustomerInformationServiceTest.java
@@ -1,65 +1,69 @@
 package com.learn.training;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import com.learn.training.FundTransfer.Controller.CustomRegistrationController;
-import com.learn.training.FundTransfer.Model.BankAccountInfo;
-import com.learn.training.FundTransfer.Model.Beneficiary;
+import com.learn.training.FundTransfer.DTO.CustomerDTO;
 import com.learn.training.FundTransfer.Model.Customer;
 import com.learn.training.FundTransfer.Repository.CustomerInformationRepository;
 import com.learn.training.FundTransfer.Service.CustomerInformationService;
 
 @ExtendWith(MockitoExtension.class)
+@TestInstance(Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class CustomerInformationServiceTest {
-
 	@Mock
-	private CustomerInformationService customerService;
-
-	@Mock
-	private CustomRegistrationController controller;
-
+	CustomerInformationService customerservice;
 	@Mock
 	CustomerInformationRepository repo;
-
-	static Customer customerinfo;
-
-static Beneficiary beneficiary;
-
-static BankAccountInfo bankaccounts;
-
-	@BeforeAll
-	public static void setUp() {
-
-		customerinfo = new Customer();
-		customerinfo.setAddress("hyd");
-		customerinfo.setAdharNumber((long) 123567890);
-		customerinfo.setEmailId("vin@gmail.com");
-		customerinfo.setMobileNumber((long) 995966850);
-		customerinfo.setUserId(12);
-		customerinfo.setUserName("vineeth");
-
-		
-
-	}
-
-	@Test
-	@DisplayName("Save customer   :: Postive Scenario")
-	public void createcustomer() {
-		ResponseEntity<Customer> response = controller.CreateCustomerInfo(customerinfo);
-		verify(customerService).CreateCustomerInfo(customerinfo);
-		assertEquals(HttpStatus.OK, controller.CreateCustomerInfo(customerinfo).getStatusCode());
-		
-	}
-	}
-
 	
+	@InjectMocks
+	CustomRegistrationController customercontroller;
+	
+	
+	static Customer customer;
+	static CustomerDTO customerdto;
+	
+	@BeforeAll
+	public void setUp() {
+		customerdto =new CustomerDTO();
+		
+		customerdto.setAddress("Hyderabad");
+		customerdto.setAdharNumber("123456789");
+		customerdto.setEmailId("vineeth@gmail.com");
+		customerdto.setMobileNo("9959668508");
+		customerdto.setState("Telangana");
+		customerdto.setUserName("vineeth");
+		
+	}
+		@Test
+		@DisplayName("Customer creation :: Postive Scenario")
+		@Order(1)
+		public void customercreationtest() {
+		when(customerservice.CreateCustomerInfo(customerdto)).thenReturn(customerdto);
+			ResponseEntity<?> result = customercontroller.CreateCustomerInfo(customerdto);
+			assertEquals(HttpStatus.OK, result.getStatusCode());
+		}
+		
+	
+}
+	
+	
+
+
