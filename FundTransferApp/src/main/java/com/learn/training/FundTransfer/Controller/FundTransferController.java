9b6ba995382de4409b87b40f6b8094fd9b4bd938
Vineethrao-77 <88935971+Vineethrao-77@users.noreply.github.com>
2021-08-30 15:58:16 +0530

    Update FundTransferController.java

--- FundTransferApp/src/main/java/com/learn/training/FundTransfer/Controller/FundTransferController.java
+++ FundTransferApp/src/main/java/com/learn/training/FundTransfer/Controller/FundTransferController.java
@@ -2,12 +2,13 @@ package com.learn.training.FundTransfer.Controller;
 
 import java.util.List;
 
-import javax.transaction.Transaction;
+import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,40 +17,44 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.learn.training.FundTransfer.Exceptions.BankAccountNotFoundException;
 import com.learn.training.FundTransfer.Exceptions.TransferFundException;
-import com.learn.training.FundTransfer.Model.BankAccountInfo;
+import com.learn.training.FundTransfer.Model.BankAccount;
 import com.learn.training.FundTransfer.Model.Response;
 import com.learn.training.FundTransfer.Model.TransferBalanceRequest;
 import com.learn.training.FundTransfer.Service.FundTransferringService;
 
 @RestController
 public class FundTransferController {
+	private static final Logger logger = LoggerFactory.getLogger(BeneficiaryController.class);
 
 	@Autowired
 	FundTransferringService service;
 	
 	@RequestMapping(value = "/createbankAccountInfo", method = RequestMethod.POST)
-	public ResponseEntity<BankAccountInfo> createBankAccount(@RequestBody BankAccountInfo accountinfo){
-		BankAccountInfo bankinfo=service.CreateBankAccount(accountinfo);
+	public ResponseEntity<BankAccount> createBankAccount(@Valid @RequestBody BankAccount accountinfo){
+		BankAccount bankinfo=service.CreateBankAccount(accountinfo);
+		logger.info("Inside the createbankAccountInfo method ");
 		return ResponseEntity.status(HttpStatus.CREATED).body(bankinfo);
 	}
 
 
 
 	@RequestMapping(value = "/getallbankAccountDetails", method = RequestMethod.GET)
-	public ResponseEntity<List<BankAccountInfo>> getAllAccountDetails() throws BankAccountNotFoundException{
-		List<BankAccountInfo> getaccounts=service.getAllAccounts();
+	public ResponseEntity<List<BankAccount>> getAllAccountDetails() throws BankAccountNotFoundException{
+		List<BankAccount> getaccounts=service.getAllAccounts();
 		if(getaccounts==null)
 			throw new BankAccountNotFoundException("unable to find the bank acocunts with the account no"+getaccounts);
-	
+	logger.info("Inside the GetAll BankAccount Details method");
 		return ResponseEntity.status(HttpStatus.OK).body(getaccounts);
 	}
 
 	
-	@PutMapping("/sendmoney")
+	@PutMapping("/fundTransfer")
 	public ResponseEntity<ResponseEntity<Response>> SendMoney(
 			@RequestBody TransferBalanceRequest transferBalanceRequest) throws TransferFundException {
 
 		ResponseEntity<Response> transfer = service.TransferBalanceRequest(transferBalanceRequest);
+		
+		logger.info("Inside the Transfer Fund Method");
 		return ResponseEntity.status(HttpStatus.OK).body(transfer);
 
 	}
