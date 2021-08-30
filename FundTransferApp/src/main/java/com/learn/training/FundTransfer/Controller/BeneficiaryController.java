44fc00a8d4324ad176f89f09fe1668c634d63ddc
Vineethrao-77 <88935971+Vineethrao-77@users.noreply.github.com>
2021-08-30 15:57:08 +0530

    Update BeneficiaryController.java

--- FundTransferApp/src/main/java/com/learn/training/FundTransfer/Controller/BeneficiaryController.java
+++ FundTransferApp/src/main/java/com/learn/training/FundTransfer/Controller/BeneficiaryController.java
@@ -2,10 +2,13 @@ package com.learn.training.FundTransfer.Controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,21 +16,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.google.common.base.Optional;
-import com.learn.training.FundTransfer.Exceptions.BankAccountNotFoundException;
 import com.learn.training.FundTransfer.Exceptions.BeneficiaryNotFoundException;
-import com.learn.training.FundTransfer.Model.BankAccountInfo;
 import com.learn.training.FundTransfer.Model.Beneficiary;
 import com.learn.training.FundTransfer.Service.BeneficiaryService;
 
 @RestController
 public class BeneficiaryController {
+
+	private static final Logger logger = LoggerFactory.getLogger(BeneficiaryController.class);
 	@Autowired
 	BeneficiaryService service;
 
 	@PostMapping("/CreateBeneficiary")
-	public ResponseEntity<Beneficiary> createBeneficiary(@RequestBody Beneficiary beneficiary) {
+	public ResponseEntity<Beneficiary> createBeneficiary(@Valid @RequestBody Beneficiary beneficiary) {
 		Beneficiary user = service.CreateBeneficiary(beneficiary);
+		if (user == null)
+			throw new BeneficiaryNotFoundException("unable to create Beneficairy");
+		logger.info("Inside the CreateBeneficiary Account Method");
 		return ResponseEntity.status(HttpStatus.CREATED).body(user);
 
 	}
@@ -36,15 +41,16 @@ public class BeneficiaryController {
 	public ResponseEntity<List<Beneficiary>> getAllAccountDetails() throws BeneficiaryNotFoundException {
 		List<Beneficiary> getaccounts = service.GetAllBeneficiarys();
 		if (getaccounts == null)
-			throw new BeneficiaryNotFoundException
-					("unable to find the beneficiary acocunts with the account no" + getaccounts);
-
+			throw new BeneficiaryNotFoundException(
+					"unable to find the beneficiary acocunts with the account no" + getaccounts);
+		logger.info("Inside the getAllBeneficiaryAccountDetails method");
 		return ResponseEntity.status(HttpStatus.OK).body(getaccounts);
 	}
 
-	@RequestMapping(value = "/deleteBeneficiary/{beneficiaryId}",method=RequestMethod.DELETE)
+	@RequestMapping(value = "/deleteBeneficiary/{beneficiaryId}", method = RequestMethod.DELETE)
 	public HttpStatus deleteBeneficiary(@PathVariable Integer beneficiaryId) {
 		service.DeleteBeneficiary(beneficiaryId);
+		logger.info("Inside the DeleteBeneficiary Method");
 		return HttpStatus.ACCEPTED;
 	}
 
